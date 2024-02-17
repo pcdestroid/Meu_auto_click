a721901759285e86d7fa0c82f89c9d343da44959
Alan Francis <alanfrancisangra@gmail.com>
2024-02-17 20:34:21 -0300

    Meu auto click

--- auto-click.py
+++ auto-click.py
@@ -6,6 +6,7 @@ print("Pressione F8 para iniciar ou parar os cliques do mouse.")
 
 clicando = False
 
+
 def on_f8_pressed():
     global clicando
     clicando = not clicando
@@ -13,6 +14,7 @@ def on_f8_pressed():
         print("Clicando. Pressione F8 para parar.")
     else:
         print("Parado. Pressione F8 para iniciar os cliques novamente.")
+
 
 # Configura o evento para escutar a tecla F8
 keyboard.add_hotkey("F8", on_f8_pressed)
@@ -23,6 +25,6 @@ try:
         time.sleep(0.1)  # Pequena pausa para evitar uso excessivo da CPU
         if clicando:
             pyautogui.click(button='right')
-            time.sleep(0.3)  # Espera 1 segundo entre os cliques
+            time.sleep(0.3)  # Espera 0.3 segundo entre os cliques
 except KeyboardInterrupt:
     print("Script interrompido.")
